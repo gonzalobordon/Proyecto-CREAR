e92d68e968814b51c553ca78ed114c53418b1f45
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-12 21:41:20 -0300

    Ahora con Vicuña
    Mejora de movimiento de personaje y una vicuña de fondo

---
+++ Assets/Scripts/PlayerTestMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestMovement : MonoBehaviour {
+
+	public Transform pj;
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		pj=GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pj.position = new Vector2(pj.position.x + Input.GetAxis("Horizontal")*(speed/10),pj.position.y);
+	}
+}
