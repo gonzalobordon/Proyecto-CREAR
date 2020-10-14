2c14c6d24f2f6389d43a15486228f1ae7ee3886a
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-14 00:36:45 -0300

    Mecanicas
    Mecanicas con animacion

--- Assets/Scripts/PlayerTestMovement.cs
+++ Assets/Scripts/PlayerTestMovement.cs
@@ -6,14 +6,17 @@ public class PlayerTestMovement : MonoBehaviour {
 
 	public Transform pj;
 	public float speed = 1f;
-
+	public Animator anim;
 	// Use this for initialization
 	void Start () {
+		anim=GetComponent<Animator>();
 		pj=GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetAxis("Horizontal")!=0)anim.SetBool("run",true);
+		else anim.SetBool("run",false);
 		pj.position = new Vector2(pj.position.x + Input.GetAxis("Horizontal")*(speed/10),pj.position.y);
 	}
 }
