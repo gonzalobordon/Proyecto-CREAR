1b9ae387f345bee7eff1b131316438e88cb31a5d
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-15 18:18:57 -0300

    Cambio Escenario
    Trigger en Casa para cambiar escenario

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager gamecontroller;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+}
