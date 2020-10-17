c1f56b785c76f8961c064e83cf33b9d1704cf311
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-16 22:15:39 -0300

    Mecanica Movimiento Mapa
    Movimiento en todo el mapa integrado

---
+++ Assets/Scripts/IngresoEscenaDer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IngresoEscenaDer : MonoBehaviour {
+
+	public string escena;
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player")
+		{
+			SceneManager.LoadScene(escena,LoadSceneMode.Single);
+			other.transform.position = new Vector2(-14f,-1.25f);
+		}
+	}
+}
