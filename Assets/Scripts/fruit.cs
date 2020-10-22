d0afd959dba02b57b42f2b20939d118afbd95f01
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-22 15:58:21 -0300

    Nuevo Minijuego
    "Catch the fruit" añadido

---
+++ Assets/Scripts/fruit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruit : MonoBehaviour {
+
+	public float speed = 1f;
+	public float lifetime = 3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector2 (gameObject.transform.position.x,gameObject.transform.position.y-(speed/10));
+		DestroyObject(gameObject,3);
+	}
+}
---
+++ Assets/Scripts/fruitcontrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruitcontrol : MonoBehaviour {
+	public GameObject fruta;
+	public float tempo = 65f;
+	public float offset = 3f;
+	public float bpm;
+	public float timing;
+	public int cont=0;
+	// Use this for initialization
+	void Start () {
+		bpm = 60/tempo;
+		timing = bpm + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timing>0) timing -= Time.deltaTime;
+		else{
+			cont+=1;
+			Debug.Log(cont);
+			gameObject.transform.position = new Vector2(Random.Range(-8,8),gameObject.transform.position.y);
+			Instantiate(fruta,gameObject.transform.position,gameObject.transform.rotation);
+			timing = bpm;
+		}
+	}
+}
