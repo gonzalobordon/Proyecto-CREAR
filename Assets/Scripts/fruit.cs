42b55418f2b39d81a6e89919f517009322eed111
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-22 21:00:37 -0300

    CTF modified
    frutas con rotacion

--- Assets/Scripts/fruit.cs
+++ Assets/Scripts/fruit.cs
@@ -15,5 +15,6 @@ public class fruit : MonoBehaviour {
 	void Update () {
 		gameObject.transform.position = new Vector2 (gameObject.transform.position.x,gameObject.transform.position.y-(speed/10));
 		DestroyObject(gameObject,3);
+		transform.Rotate(new Vector3(0,0,speed));
 	}
 }
