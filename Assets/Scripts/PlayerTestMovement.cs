adb1a26c63877e05af26ec02cabf6af67d51e271
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-23 13:23:31 -0300

    cambios menores

--- Assets/Scripts/PlayerTestMovement.cs
+++ Assets/Scripts/PlayerTestMovement.cs
@@ -8,26 +8,25 @@ public class PlayerTestMovement : MonoBehaviour {
 	public float speed = 1f;
 	public float limitposx = 16f;
 	public float limitposy = 7f;
-	public Animator anim;
+	//public Animator anim;
 	public bool omnidir = false;
-	public bool endless = false;
 	// Use this for initialization
 	void Start () {
-		anim=GetComponent<Animator>();
+		//anim=GetComponent<Animator>();
 		pj=GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (endless==false)
-		{
-			if(Input.GetAxis("Horizontal")!=0)anim.SetBool("run",true);	else anim.SetBool("run",false);
-			if(Input.GetAxis("Horizontal")>0) pj.localScale = new Vector2(1,1);
-			if(Input.GetAxis("Horizontal")<0) pj.localScale = new Vector2(-1,1);
-			if(Input.GetAxis("Vertical")>0 & omnidir==true)anim.SetBool("up",true); else anim.SetBool("up",false);
-			if(Input.GetAxis("Vertical")<0 & omnidir==true)anim.SetBool("down",true); else anim.SetBool("down",false);
-			if (omnidir) pj.position = new Vector2(Mathf.Clamp(pj.position.x + Input.GetAxis("Horizontal")*(speed/10),-limitposx,limitposx) ,Mathf.Clamp(pj.position.y + Input.GetAxis("Vertical")*(speed/10),-limitposy,limitposy));
-				else pj.position = new Vector2(Mathf.Clamp(pj.position.x + Input.GetAxis("Horizontal")*(speed/10),-limitposx,limitposx) ,pj.position.y);	
-		}else anim.SetBool("run",true);
+		//if(Input.GetAxis("Horizontal")!=0)anim.SetBool("run",true);	else anim.SetBool("run",false);
+		if(Input.GetAxis("Horizontal")>0) pj.localScale = new Vector2(1,1);			
+		if(Input.GetAxis("Horizontal")<0) pj.localScale = new Vector2(-1,1);
+		pj.position = new Vector2(Mathf.Clamp(pj.position.x + Input.GetAxis("Horizontal")*(speed/10),-limitposx,limitposx) ,pj.position.y);	
+
+		if (omnidir==true){
+		//	if(Input.GetAxis("Vertical")>0)anim.SetBool("up",true); else anim.SetBool("up",false);
+		//	if(Input.GetAxis("Vertical")<0)anim.SetBool("down",true); else anim.SetBool("down",false);
+			pj.position = new Vector2(pj.position.x,Mathf.Clamp(pj.position.y + Input.GetAxis("Vertical")*(speed/10),-limitposy,limitposy));
+		} 
 	}
 }
