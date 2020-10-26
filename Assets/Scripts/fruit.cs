ea1380869c869f3b90f7f6917808153a669b5d4d
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-26 19:57:31 -0300

    Nuevo Juego Añadido
    dos minijuegos nuevos

--- Assets/Scripts/fruit.cs
+++ Assets/Scripts/fruit.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class fruit : MonoBehaviour {
 
-	public float speed = 1f;
-	public float lifetime = 3f;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+	public float speedX = 1f;
+	public float speedY = 0f;
+	public float speedrot = 1f;
 	void Update () {
-		gameObject.transform.position = new Vector2 (gameObject.transform.position.x,gameObject.transform.position.y-(speed/10));
-		DestroyObject(gameObject,3);
-		transform.Rotate(new Vector3(0,0,speed));
+		gameObject.transform.position = new Vector2 (gameObject.transform.position.x+(speedX/10),gameObject.transform.position.y+(speedY/10));
+		transform.Rotate(new Vector3(0,0,speedrot));
 	}
 }
