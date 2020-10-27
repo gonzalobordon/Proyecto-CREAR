c95a30fe80a3f2dab54e68552dd12a2ca78c88fd
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-26 23:43:22 -0300

    Revert "Nuevo Juego Añadido"
    This reverts commit 6e2a51fbe9d8065051f7239bfd29682b477a951c.

--- Assets/Scripts/IngresoEscena.cs
+++ Assets/Scripts/IngresoEscena.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class IngresoEscena : MonoBehaviour {
 	public string escena;
-	public bool button = false;
-	public string namebutton = "Fire1";
 	public float posx = 0f;
 	public float posy = 0f; 
-	void OnTriggerStay2D(Collider2D other){
+	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.tag == "Player")
 		{
-			if (button==true)
-			{	
-				if (Input.GetButtonDown(namebutton))
-				{
-					SceneManager.LoadScene(escena,LoadSceneMode.Single);
-					other.transform.position = new Vector2(posx,posy);	
-				}
-			}else{
-					SceneManager.LoadScene(escena,LoadSceneMode.Single);
-					other.transform.position = new Vector2(posx,posy);
-			} 
+			SceneManager.LoadScene(escena,LoadSceneMode.Single);
+			other.transform.position = new Vector2(posx,posy);
 		}
 	}
 }
