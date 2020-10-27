6e2a51fbe9d8065051f7239bfd29682b477a951c
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-26 23:41:06 -0300

    Nuevo Juego Añadido
    dos minijuegos nuevos

--- Assets/Scripts/IngresoEscena.cs
+++ Assets/Scripts/IngresoEscena.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class IngresoEscena : MonoBehaviour {
 	public string escena;
+	public bool button = false;
+	public string namebutton = "Fire1";
 	public float posx = 0f;
 	public float posy = 0f; 
-	void OnTriggerEnter2D(Collider2D other){
+	void OnTriggerStay2D(Collider2D other){
 		if (other.gameObject.tag == "Player")
 		{
-			SceneManager.LoadScene(escena,LoadSceneMode.Single);
-			other.transform.position = new Vector2(posx,posy);
+			if (button==true)
+			{	
+				if (Input.GetButtonDown(namebutton))
+				{
+					SceneManager.LoadScene(escena,LoadSceneMode.Single);
+					other.transform.position = new Vector2(posx,posy);	
+				}
+			}else{
+					SceneManager.LoadScene(escena,LoadSceneMode.Single);
+					other.transform.position = new Vector2(posx,posy);
+			} 
 		}
 	}
 }
