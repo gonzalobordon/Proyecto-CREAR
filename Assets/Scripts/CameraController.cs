c6e635ab31e6f7b6b290244e7a84f8e798adaab6
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-30 02:46:19 -0300

    camera controller mejorado

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+
+public class CameraController : MonoBehaviour {
+	public Transform target;
+	[Range(0f,1f)]
+	public float velocity = 0.25f;
+	[Header("Limite Vertical")]
+	[Tooltip("Limite Superior")]
+	public float maxY = 9f;
+	[Tooltip("Limite Inferior")]
+	public float minY = -9f;
+	[Header("Limite Horizontal")]
+	[Tooltip("Limite Izquierdo")]
+	public float minX = 10f;
+	[Tooltip("Limite Derecho")]
+	public float maxX = -10f;
+	void FixedUpdate () {
+		Vector3 targetpos = new Vector3(
+										Mathf.Clamp(target.position.x,minX,maxX),
+										Mathf.Clamp(target.position.y,minY,maxY),
+										transform.position.z
+										);
+		transform.position = Vector3.Lerp(transform.position,targetpos,velocity);
+	}
+}
--- Assets/Scripts/fruit.cs
+++ Assets/Scripts/fruit.cs
@@ -7,8 +7,14 @@ public class fruit : MonoBehaviour {
 	public float speedX = 1f;
 	public float speedY = 0f;
 	public float speedrot = 1f;
+	public bool deleteable = false;
+	public float lifetime = 5f;
 	void Update () {
 		gameObject.transform.position = new Vector2 (gameObject.transform.position.x+(speedX/10),gameObject.transform.position.y+(speedY/10));
 		transform.Rotate(new Vector3(0,0,speedrot));
+		if (deleteable)
+		{
+			DestroyObject(gameObject,lifetime);
+		}
 	}
 }
