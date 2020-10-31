1df66dde1bc3130f53a8988a404eefbf117c99aa
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2020-10-31 19:23:06 -0300

    Nueva actualizacion Escenas
    Escenarios y personaje cambiados

--- Assets/Scripts/IngresoEscena.cs
+++ Assets/Scripts/IngresoEscena.cs
@@ -8,11 +8,13 @@ public class IngresoEscena : MonoBehaviour {
 	public bool button = false;
 	public string namebutton = "Fire1";
 	public float posx = 0f;
-	public float posy = 0f; 
+	public float posy = 0f;
+	public bool msge = false;
+	public GameObject msgr;
 	void OnTriggerStay2D(Collider2D other){
 		if (other.gameObject.tag == "Player")
 		{
-			if (button==true)
+			if (button)
 			{	
 				if (Input.GetButtonDown(namebutton))
 				{
@@ -22,37 +24,78 @@ public class IngresoEscena : MonoBehaviour {
 			}else{
 					SceneManager.LoadScene(escena,LoadSceneMode.Single);
 					other.transform.position = new Vector2(posx,posy);
-			} 
+			}
 		}
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag=="Player"){
+			if (msge && msgr!=null) msgr.SetActive(true);
+		}	
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.tag=="Player"){
+			if(msge && msgr!=null) msgr.SetActive(false);
 	}
 }
 
 
 [CustomEditor(typeof(IngresoEscena))]
 public class IngresoEscenaEditor : Editor{
-	private GUILayoutOption[] options = {GUILayout.MaxWidth(70.0f),GUILayout.MinWidth(20.0f)};
+	public bool mostrarpanel;
+	public int index = 0;
+	public string[] inputs = new string[]{
+		"Fire1","Fire2","Fire3","Jump"
+	};
+	private GUILayoutOption[] options = {GUILayout.MaxWidth(150.0f),GUILayout.MinWidth(80.0f)};
 	override public void OnInspectorGUI(){
 		var IngresoEscena = target as IngresoEscena;
 
 		EditorGUILayout.BeginHorizontal();
-		GUILayout.Label("Escena a ingresar: ");
-		IngresoEscena.escena = EditorGUILayout.TextField(IngresoEscena.escena);
+		IngresoEscena.escena = EditorGUILayout.TextField("Escena a ingresar: ",IngresoEscena.escena);
 		EditorGUILayout.EndHorizontal();
 
+		EditorGUILayout.LabelField("Posicion Inicial");
 		EditorGUILayout.BeginHorizontal();
-		GUILayout.Label("Posicion en escena: ");
-		GUILayout.Label("x");
-		IngresoEscena.posx = EditorGUILayout.FloatField(IngresoEscena.posx,options);
-		GUILayout.Label("y");
-		IngresoEscena.posy = EditorGUILayout.FloatField(IngresoEscena.posy,options);
+		GUILayout.Label("X");
+		IngresoEscena.posx = EditorGUILayout.FloatField(IngresoEscena.posx);
+		GUILayout.Label("Y");
+		IngresoEscena.posy = EditorGUILayout.FloatField(IngresoEscena.posy);
 		EditorGUILayout.EndHorizontal();
 
-		EditorGUILayout.BeginHorizontal();
-		IngresoEscena.button = GUILayout.Toggle(IngresoEscena.button,"Presionar botón");
-		if (IngresoEscena.button)
+		mostrarpanel = EditorGUILayout.Foldout(mostrarpanel,"Opciones");
+
+		if (mostrarpanel)
 		{
-			IngresoEscena.namebutton = EditorGUILayout.TextField(IngresoEscena.namebutton);
+			EditorGUILayout.BeginHorizontal();
+			IngresoEscena.button = GUILayout.Toggle(IngresoEscena.button,"Presionar botón");
+			if (IngresoEscena.button)
+			{
+				index = EditorGUILayout.Popup(index,inputs);
+
+				switch (index)
+				{
+					case 0: IngresoEscena.namebutton = inputs[0];
+					break;
+					case 1: IngresoEscena.namebutton = inputs[1];
+					break;
+					case 2: IngresoEscena.namebutton = inputs[2];
+					break;
+					case 3: IngresoEscena.namebutton = inputs[3];
+					break;
+				}
+			}
+			EditorGUILayout.EndHorizontal();
+
+			EditorGUILayout.BeginHorizontal();
+			IngresoEscena.msge = GUILayout.Toggle(IngresoEscena.msge,"Objeto Mensaje");
+			if (IngresoEscena.msge)
+			{
+				IngresoEscena.msgr = (GameObject)EditorGUILayout.ObjectField(IngresoEscena.msgr,typeof(GameObject),true);
+			}
+			EditorGUILayout.EndHorizontal();
 		}
-		EditorGUILayout.EndHorizontal();
+
+		}
 	}
 }
---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Runner : MonoBehaviour {
+	public int modo = 0;
+	public float[] speed = {5f,5f,5f};
+	public float[] jump = {20f,20f,0f};
+	public bool onjump = false;
+	public bool enPiso;
+	public float[] gravity = {9.8f,9.8f,0f};
+	public Rigidbody2D rb;
+	public Animator anim;
+	private SpriteRenderer spr;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		spr = GetComponent<SpriteRenderer>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		switch (other.gameObject.tag)
+		{
+			case "Scroll" :
+				modo = 0;
+			break;
+			case "Runner" :
+				modo = 1;
+			break;
+			case "TopDown" :
+				modo = 2;
+			break;
+		}
+	}
+
+	void FixedUpdate () {
+		rb.gravityScale = gravity[modo];
+		enPiso = Physics2D.OverlapCircle(new Vector2(gameObject.transform.position.x,(gameObject.transform.position.y)-1.6f*gameObject.transform.localScale.y),0.25f,1<<8);
+		rb.velocity = new Vector2(Input.GetAxis("Horizontal")*speed[modo],rb.velocity.y);
+		switch (modo)
+		{
+			case 0:	if (Input.GetButton("Jump") && enPiso) rb.velocity = new Vector2(rb.velocity.x,jump[modo]);
+					if(Input.GetAxis("Horizontal")>0) spr.flipX = false;
+					if(Input.GetAxis("Horizontal")<0) spr.flipX = true;
+					if (enPiso && Input.GetAxis("Horizontal")!=0) anim.SetBool("run",true);
+					else anim.SetBool("run",false);
+			break;
+			case 1:	
+					spr.flipX = false;
+					if (enPiso) 
+					{	
+						anim.SetBool("run",true);
+						if (Input.GetButtonDown("Jump")){
+						onjump = true;
+						rb.velocity = new Vector2(rb.velocity.x,jump[modo]);
+						}
+					}else{
+						anim.SetBool("run",false);
+						if (Input.GetButtonDown("Jump") && onjump)
+						{
+						onjump = false;
+						rb.velocity = new Vector2(rb.velocity.x,jump[modo]);	
+						} 
+					}
+			break;
+			case 2:
+					if(Input.GetAxis("Horizontal")>0) spr.flipX = false;
+					if(Input.GetAxis("Horizontal")<0) spr.flipX = true;
+					rb.velocity = new Vector2(rb.velocity.x,Input.GetAxis("Vertical")*speed[modo]);
+					if(Input.GetAxis("Horizontal")!=0) anim.SetBool("run",true);
+					else anim.SetBool("run",false);
+			break;
+			default:if (Input.GetButton("Jump") && enPiso) rb.velocity = new Vector2(rb.velocity.x,jump[modo]);
+					if(Input.GetAxis("Horizontal")>0) spr.flipX = false;
+					if(Input.GetAxis("Horizontal")<0) spr.flipX = true;
+					if (enPiso && Input.GetAxis("Horizontal")!=0) anim.SetBool("run",true);
+					else anim.SetBool("run",false);
+			break;
+		}
+	}
+}
+
+[CustomEditor(typeof(Runner))]
+public class RunnerEditor : Editor{
+	public string[] modes = new string[]{"Scroll Lateral","Infite Runner","TopDown"};
+    public override void OnInspectorGUI()
+    {
+        var Runner = target as Runner;
+		EditorGUILayout.LabelField("Tipo de Movimiento");
+		Runner.modo = EditorGUILayout.Popup(Runner.modo,modes);
+		Runner.speed[Runner.modo] = EditorGUILayout.Slider("Velocidad",Runner.speed[Runner.modo],0f,30f);
+		Runner.jump[Runner.modo] = EditorGUILayout.Slider("Fuerza de Salto",Runner.jump[Runner.modo],0f,50f);
+		Runner.gravity[Runner.modo] = EditorGUILayout.Slider("Gravedad",Runner.gravity[Runner.modo],0f,9.8f);
+    }
+}
